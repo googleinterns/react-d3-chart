827a803aaaa8513cfdfc3c37915678a9d821305c
Craig Lewis <craigericlewis@gmail.com>
2020-07-21 14:26:53 -0400

    Init project, react and storybook

---
+++ react-d3-chart/src/components/MyComponent.stories.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import { MyComponent } from "./MyComponent";
+storiesOf("Select | Catalog", module).add("Default", () => <MyComponent />);
