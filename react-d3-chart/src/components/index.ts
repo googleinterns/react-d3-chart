97efd08ecee6a3fb0293a4e4c1f6cc4d9b5c5a63
hanmeow <hanmeow@google.com>
2020-09-14 14:43:34 -0700

    export new components; bump version

--- react-d3-chart/src/components/index.ts
+++ react-d3-chart/src/components/index.ts
@@ -1,7 +1,11 @@
 export * from './Axis';
 export * from './Bisector';
 export * from './Context';
+export * from './HeatMap';
 export * from './Line';
 export * from './LineChart';
 export * from './LineContainer';
+export * from './Scroller';
+export * from './Tooltip';
 export * from './Overlay';
+
